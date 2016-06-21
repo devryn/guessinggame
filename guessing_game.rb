5f23626eb5323f29ca2da5f0bf4056f087b60a17
Kate Walters <kawalters01@gmail.com>
2016-06-21 15:42:37 -0500

    split into one class

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,37 +1,47 @@
-puts "Guess a number, any number, so long as it's between 1-100."
 
-comp_answer = (1..100).to_a.sample
-#puts comp_answer
-guesses = 0
-prev_answers = []
 
-loop do
-  guesses += 1
-  player_answer = gets.chomp.to_i
 
-  puts "Previous answers: #{prev_answers}"
-  if prev_answers.include? player_answer
-    puts "You already tried that. You feeling okay?"
-  elsif player_answer == comp_answer
-    puts "Winner winner chicken dinner"
-    puts "It took #{guesses} guesses."
-    exit
-  elsif player_answer == 0
-    puts "Pick a number between 1-100."
-  elsif player_answer > comp_answer
-    puts "Too high, too high!"
-  elsif player_answer < comp_answer
-    puts "Eh, try bumping it up a little."
-  else player_answer = player_answer
-    puts "Are you feeling okay?"
+
+
+class Game
+  def start
+    puts "Guess a number, any number, so long as it's between 1-100."
+
+    comp_answer = (1..100).to_a.sample
+    guesses = 0
+    prev_answers = []
+
+    loop do
+      guesses += 1
+      player_answer = gets.chomp.to_i
+
+      puts "Previous answers: #{prev_answers}"
+      if prev_answers.include? player_answer
+        puts "You already tried that. You feeling okay?"
+      elsif player_answer == comp_answer
+        puts "Winner winner chicken dinner"
+        puts "It took #{guesses} guesses."
+        exit
+      elsif player_answer == 0
+        puts "Pick a number between 1-100."
+      elsif player_answer > comp_answer
+        puts "Too high, too high!"
+      elsif player_answer < comp_answer
+        puts "Eh, try bumping it up a little."
+      else player_answer = player_answer
+        puts "Are you feeling okay?"
+      end
+
+    unless prev_answers.include? player_answer
+      prev_answers.push player_answer
+    end
+
+      if guesses >= 5
+        puts "You took too long. Bye!"
+        break
+      end
+    end
   end
-
-unless prev_answers.include? player_answer
-  prev_answers.push player_answer
 end
 
-  if guesses >= 5
-    puts "You took too long. Bye!"
-    break
-  end
-end
+Game.new.start
