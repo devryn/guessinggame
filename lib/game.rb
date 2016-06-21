b87a039e0d11213a30490f5e3aff1a0f256b4855
Kate Walters <kawalters01@gmail.com>
2016-06-21 16:26:25 -0500

    final refactoring

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,7 @@
 class Game
   PREV = "You already tried that. You feeling okay?"
   WIN = "Winner winner chicken dinner"
-  INVALID = "Pick a number between 1-100."
+  INVALID = "Wrong. Pick a number between 1-100."
   HIGH = "Too high, too high!"
   LOW = "Eh, try bumping it up a little."
   MAX = "You took too long. Bye!"
@@ -21,21 +21,37 @@ class Game
     puts "It took #{guesses} guesses."
   end
 
+  def correct_answer(player_answer, comp_answer)
+    player_answer == comp_answer
+  end
+
+  def too_high (player_answer, comp_answer)
+    player_answer > comp_answer
+  end
+
+  def too_low(player_answer, comp_answer)
+    player_answer < comp_answer
+  end
+
+  def invalid(player_answer)
+    player_answer == 0
+  end
+
     loop do
       guesses += 1
       player_answer = gets.chomp.to_i
       prev_string(prev_answers)
       if prev_answers.include? player_answer
         puts PREV
-      elsif player_answer == comp_answer
+      elsif correct_answer(player_answer, comp_answer)
         puts WIN
         num_guesses(guesses)
         exit
-      elsif player_answer == 0
+      elsif invalid(player_answer)
         puts INVALID
-      elsif player_answer > comp_answer
+      elsif too_high(player_answer, comp_answer)
         puts HIGH
-      elsif player_answer < comp_answer
+      elsif too_low(player_answer, comp_answer)
         puts LOW
       end
 
