99140cb029d5882fa7de583f319cbc9fafc34ff4
Kate Walters <kawalters01@gmail.com>
2016-06-21 14:46:47 -0500

    need duplicate user input method

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,21 +1,28 @@
 puts "Guess a number, any number, so long as it's between 1-100."
 
-player_answer = gets.chomp.to_i
 comp_answer = (1..100).to_a.sample
 puts comp_answer
+guesses = 0
 
 loop do
+  guesses += 1
+  player_answer = gets.chomp.to_i
   if player_answer == comp_answer
     puts "Winner winner chicken dinner"
+    puts "It took #{guesses} tries."
     exit
+  elsif player_answer == 0
+    puts "Pick a number between 1-100."
   elsif player_answer > comp_answer
     puts "Too high, too high!"
-    player_answer = gets.chomp.to_i
   elsif player_answer < comp_answer
     puts "Eh, try bumping it up a little."
-    player_answer = gets.chomp.to_i
-  else
+  else player_answer = player_answer
     puts "Are you feeling okay?"
-    player_answer = gets.chomp.to_i
+  end
+
+  if guesses >= 5
+    puts "You took too long. Bye!"
+    break
   end
 end
